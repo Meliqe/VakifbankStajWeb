5208344ab65b012d7c68855fe1fffff6035675b6
Meliqe <55erebor55@gmail.com>
2024-11-16 19:48:02 +0300

    gereksiz yorum satırları silindi

--- src/test/java/pages/HepsiburadaPage.java
+++ src/test/java/pages/HepsiburadaPage.java
@@ -1,11 +1,8 @@
 package pages;
 import org.openqa.selenium.By;
 import BaseFile.BaseMethods;
-import utility.DriverManager;
 
 public class HepsiburadaPage extends BaseMethods {
-
-    // Sabit XPath'ler
     private static final By ELEKTRONIK_MENU = By.xpath("//span[text()='Elektronik']");
     private static final By TELEFON_AKSESUAR_MENU = By.xpath("//a[contains(text(), 'Telefon & Telefon Aksesuarları')]");
     private static final By SAMSUNG_CATEGORY = By.xpath("//a[contains(@href, '/samsung-android-telefonlar-xc-60005201')]//span[text()='Samsung']");
@@ -13,7 +10,7 @@ public class HepsiburadaPage extends BaseMethods {
     private static final By URUN_FIYATI = By.xpath("//div[@data-test-id='price-current-price']");
 
     public HepsiburadaPage() {
-        super();  // BaseMethods'daki driver ve metodları kullan
+        super();
     }
 
     public void goToElectronicsMenu() {
--- src/test/java/utility/DriverManager.java
+++ src/test/java/utility/DriverManager.java
@@ -3,14 +3,11 @@ package utility;
 import org.openqa.selenium.WebDriver;
 
 public class DriverManager {
-    // ThreadLocal, her iş parçacığının sadece kendine özel bir WebDriver nesnesini almasını sağlar.
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
-
 
     public static void setDriver() {
         driver.set(BrowserManager.createBrowser(Hooks.BROWSERNAME));
     }
-//driver.get() ifadesi, ThreadLocal yapısı sayesinde her iş parçacığını n kendi WebDriver nesnesine erişmesini sağlar.
     public static WebDriver getDriver() {
         if (driver.get() == null) {
             setDriver();
